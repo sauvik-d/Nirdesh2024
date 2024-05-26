959c8ef9518c4dd682e910b4f43a8b912ea2a9f9
sauvik-d <sauvikdutta.nnhs.kol.e2720@gmail.com>
2024-05-26 22:39:47 +0530

    updated generator of oindrila sunil and somdutta

---
+++ oindrila/generator_oindrila.py
@@ -0,0 +1,94 @@
+import random
+
+test = random.randint(1, 10)
+print("No. of tests are: "+str(test))
+ip = open("input.txt", "w")
+op = open("output.txt", "w")
+ip.write(str(test)+'\n')
+
+def threat(arr, n, element, i, j):
+    if i == 0 or j == 0 or i == n-1 or j == n-1:
+        return False
+    left = 0
+    right  = 0
+    up = 0
+    down = 0
+    # for checking total depths
+    '''
+    for a in range(j):
+        left += arr[i][a]
+    for b in range(j+1, n):
+        right += arr[i][b]
+    for c in range(i):
+        up += arr[c][j]
+    for d in range(i+1, n):
+        down += arr[d][j]
+    '''
+    # for checking depths of only adjacent elements
+    left = arr[i][j-1]
+    right = arr[i][j+1]
+    up = arr[i-1][j]
+    down = arr[i+1][j]
+    fmt = "Depths for arr[{0}][{1}] are"
+    if left <= element and right <= element and up <= element and down <= element:
+        # print(fmt.format(i, j))
+        # print(left, right, up, down)
+        return True
+    else:
+        return False
+
+def func(n):
+    arr = []
+    for i in range(n):
+        col = []
+        for j in range(n):
+            col.append(random.randint(1, 100000))
+        arr.append(col)
+    for i in range(n):
+        for j in range(n):
+            ip.write(str(arr[i][j])+" ")
+        ip.write("\n")
+    ip.write("\n")
+    replaceCavity(n, arr)
+    # print("After removing cavity: ")
+    # for i in range(n):
+        # print(arr[i])
+
+'''
+def cavity(n, arr):
+    
+    replaceCavity(mx, n, arr)
+'''
+def replaceCavity(n, arr):
+    cavityflag = 1
+    rounds = 1
+    totalcavity = 0
+    while cavityflag != 0:
+        mx = -1
+        for i in range(n):
+            for j in range(n):
+                mx = max(mx, arr[i][j])
+        # print("Max for round " + str(rounds) +" is: "+str(mx))
+        # fmt2 = "After Round {0} Cavity Check"
+        # print(fmt2.format(rounds))
+        rounds += 1
+        flag = 0
+        for i in range(n):
+            for j in range(n):
+                if arr[i][j] == mx:
+                    if threat(arr, n, arr[i][j], i, j) == True:
+                        flag = 1
+                        arr[i][j] = 0
+                        totalcavity += 1
+        if flag == 0:
+            cavityflag = 0
+    print("The total rounds are: "+ str(rounds-1))
+    print("Total no. of Cavity removed is: " + str(totalcavity))
+
+for i in range(test):
+    print("Result at Test "+str(i+1)+":")
+    n = random.randint(50, 100)
+    # n = 100
+    # print(rounds)
+    ip.write(str(n)+'\n')
+    func(n)
--- sunil/generator_sunil.cpp
+++ sunil/generator_sunil.cpp
@@ -4,11 +4,53 @@
 #include<cstdlib>
 #include<string.h>
 #include<time.h>
+#define loop(i, n) for(int i=0;i<n;i++)
 using namespace std;
 int main(){
 	srand(time(0));
 	ofstream myfile1("input1.txt");
 	ofstream myfile2("output1.txt");
-	
+	int input;
+	cin>>input;
+	int test = (rand() % (1-1+1)) + 1;
+	cout<<test<<endl;
+	int n = (rand() % (50-5+1))+1;
+	vector<char> alphabets(26);
+	//= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int l = 97;
+	loop(i, 26){
+		alphabets[i] = char(l); l++;
+		if(l == 122) break;
+	}
+	vector<char> mainarray(n);
+	for(int i=0;i<n;i++){
+		int random = rand() % 26;
+		mainarray[i] = alphabets[random];
+	}
+	cout<<"Original Main Array is: "<<endl;
+	loop(i, n) cout<<mainarray[i]<<" ";
+	cout<<endl;
+	sort(mainarray.begin(), mainarray.end());
+	vector<char> priorarray;
+	cout<<"Sorted Main Array is: "<<endl;
+	loop(i, n) cout<<mainarray[i]<<" ";
+	cout<<endl;
+	loop(i, n-1){
+		if(mainarray[i] != mainarray[i+1]){
+			priorarray.push_back(mainarray[i]);
+		}
+	}
+	priorarray.push_back(mainarray[n-1]);
+	cout<<"Array with unique elements is: "<<endl;
+	loop(i, n) cout<<priorarray[i]<<" ";
+	cout<<endl;
+	srand(time(0));
+	vector<int> priority;
+	loop(i, priorarray.size()){
+		int random2 = rand() % priorarray.size();
+		priority.push_back(random2);
+	}
+	loop(i, priority.size()) cout<<priority[i]<<" ";
+	cout<<endl;
 	return 0;
 }
